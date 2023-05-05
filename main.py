dbe6be1bc43fd472637793eca88823eef3c8b843
Aditya Irvan <adityairvanku@gmail.com>
2023-05-05 09:53:12 +0700

    receive callback

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from src import config_lora
 from src import LoRaReceiver
 # import LoRaSetSpread
 # import LoRaSetSyncWord
-# from src import LoRaReceiverCallback
+from src import LoRaReceiverCallback
 # import LoRaDuplex
 # from src import LoRaDuplexCallback
 # import LoRaPingPong
@@ -42,10 +42,10 @@ def main():
 
     # LoRaDumpRegisters.dumpRegisters(lora)
     # LoRaSender.send(lora)
-    LoRaReceiver.receive(lora)
+    # LoRaReceiver.receive(lora)
     # LoRaSetSpread.setSpread(lora)
     # LoRaSetSyncWord.setSyncWord(lora)
-    # LoRaReceiverCallback.receiveCallback(lora)
+    LoRaReceiverCallback.receiveCallback(lora)
     # LoRaDuplex.duplex(lora)
     # LoRaDuplexCallback.duplexCallback(lora)
     # LoRaPingPong.ping_pong(lora)
