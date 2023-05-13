875db05e31fa4491415748c9b79e6749e9aa2c4d
Aditya Irvan <adityairvanku@gmail.com>
2023-05-14 00:54:37 +0700

    work

--- src/LoRaReceiver.py
+++ src/LoRaReceiver.py
@@ -10,16 +10,16 @@ from . import ascon
 import binascii
 
 load_dotenv()
-display = lcd_i2c.lcd()
-asc = ascon.Ascon()
 
 
 async def receive(lora):
+    display = lcd_i2c.lcd()
+    asc = ascon.Ascon()
     rabbitmq_server = os.getenv('RABBITMQ_SERVER')
     key = os.getenv("ENCRYPT_KEY")
     nonce = os.getenv("ENCYPT_NONCE")
     amqp_connection = amqp_controller.AMQPConnection(rabbitmq_server)
-    await connect_to_rabbitmq(amqp_connection)
+    await connect_to_rabbitmq(amqp_connection, display)
     try:
         print("LoRa Receiver")
         while True:
@@ -27,59 +27,61 @@ async def receive(lora):
                 lora.blink_led()
                 try:
                     payload = lora.read_payload()
-                    plaintext, nonce = await decryption(
+                    plaintext, nonce = decryption(
                         asc, binascii.unhexlify(payload), key, nonce, "CBC")
-                    await show_info(plaintext, lora)
+                    message = plaintext.decode("utf-8")
+                    message_json = json.loads(message)
+                    show_info(display, message_json)
+                    print("\n*** Received message ***\n{}".format(message))
+                    print("with RSSI: {}\n".format(lora.packetRssi()))
                     await amqp_connection.send_amqp_message(payload)
                 except Exception as e:
                     print(e)
     except KeyboardInterrupt:
-        print("Keyboard interrupt detected.")
         display.lcd_clear()
-        display.lcd_display_string("Exit", 1)
-        display.lcd_display_string("Conn Close", 2)
-
+        print("Keyboard interrupt detected.")
     finally:
         await amqp_connection.close()
 
 
-async def connect_to_rabbitmq(amqp_connection):
+async def connect_to_rabbitmq(amqp_connection, display):
     while True:
         try:
             await amqp_connection.connect()
             print("Connection to RabbitMQ established successfully.")
-            await show_on_lcd(["Connect RabbitMQ", "Success"])
             break
         except Exception as e:
             print(f"{e}. Retrying in 5 seconds...")
-            display.lcd_clear()
-            display.lcd_display_string("Err connect to", 1)
-            display.lcd_display_string("RabbitMQ Broker", 2)
+            show_on_lcd(display, ["Err connect to", "RabbitMQ Broker"])
             await asyncio.sleep(5)
 
 
-async def show_info(plaintext, lora):
-    message = plaintext.decode("utf-8")
-    message_json = json.loads(message)
-    th = f"T: {message_json['t']}C H: {message_json['h']}%"
-    tm = f"{await get_formatted_date(message_json['tsp'])}"
-    show_on_lcd([th, tm])
-    print(f"\n*** Received message ***\n{message_json}")
-    print(f"with RSSI: {lora.packetRssi()}\n")
+def show_info(display, message_json):
+    temp = f'T: {message_json["t"]}C'
+    hum = f'H: {message_json["h"]}%'
+    display.lcd_display_string(
+        get_formatted_date(message_json["tsp"]), 1)
+    display.lcd_display_string(
+        get_formatted_time(message_json["tsp"]), 1, 10)
+    display.lcd_display_string(temp, 2)
+    display.lcd_display_string(hum, 2, 8)
 
 
-async def show_on_lcd(items, delay=0):
-    display.lcd_clear()
+def show_on_lcd(lcd, items, delay=0):
     for x, text in enumerate(items):
-        display.lcd_display_string(text, x)
+        lcd.lcd_display_string(text, x)
     sleep(delay)
 
 
-async def get_formatted_date(date_tuple):
-    return f"{date_tuple[0]:04d}-{date_tuple[1]:02d}-{date_tuple[2]:02d} {date_tuple[4]:02d}:{date_tuple[5]:02d}"
+def get_formatted_date(date_tuple):
+    return f"{date_tuple[0]:04d}-{date_tuple[1]:02d}-{date_tuple[2]:02d}"
 
 
-async def decryption(ascon, ciphertext, key, nonce, mode="ECB"):
+def get_formatted_time(time_tuple):
+    return f"{time_tuple[4]:02d}:{time_tuple[5]:02d}"
+
+
+def decryption(ascon, ciphertext, key, nonce, mode="ECB"):
     key_bytes = key.encode('utf-8')
     if not isinstance(nonce, bytes):
         nonce = nonce.encode('utf-8')
