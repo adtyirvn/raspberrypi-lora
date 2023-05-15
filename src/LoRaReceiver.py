481ebc29f3a01e00bc3b741f578cfd9acd637089
Aditya Irvan <adityairvanku@gmail.com>
2023-05-15 15:46:07 +0700

    fix lcd

--- src/LoRaReceiver.py
+++ src/LoRaReceiver.py
@@ -24,32 +24,41 @@ nonce_g = config.ENCRYPT_NONCE
 
 
 async def receive(lora):
-    msg_count = 0
-    # display.lcd_clear()
-    await connect_to_rabbitmq(amqp_connection)
-    delay_ms = 4000
-    previous_time = int(round(time.time() * 1000))
-    # secs = 0
-    while True:
-        try:
-            current_time = int(round(time.time() * 1000))
-            elapsed_time = current_time - previous_time
-            if elapsed_time >= delay_ms:
-                mes_dict = {
-                    "idp": msg_count,
-                    "id": master_node,
-                    "to": node_one
-                }
-                if msg_count == 0:
-                    mes_dict["rst"] = "reset"
-                mes_json = json.dumps(mes_dict)
-                lora.println(mes_json)
-                msg_count += 1
-                print(f"send: {mes_json}\n")
-                previous_time = current_time
-            await on_receive(lora)
-        except Exception as e:
-            print(f"Error: {e}")
+    try:
+        msg_count = 0
+        # display.lcd_clear()
+        await connect_to_rabbitmq(amqp_connection)
+        delay_ms = 4000
+        previous_time = int(round(time.time() * 1000))
+        # secs = 0
+        while True:
+            try:
+                current_time = int(round(time.time() * 1000))
+                elapsed_time = current_time - previous_time
+                if elapsed_time >= delay_ms:
+                    mes_dict = {
+                        "idp": msg_count,
+                        "id": master_node,
+                        "to": node_one
+                    }
+                    if msg_count == 0:
+                        mes_dict["rst"] = "reset"
+                    mes_json = json.dumps(mes_dict)
+                    lora.println(mes_json)
+                    msg_count += 1
+                    print(f"send: {mes_json}\n")
+                    previous_time = current_time
+                await on_receive(lora)
+            except Exception as e:
+                print(f"Error: {e}")
+    except KeyboardInterrupt:
+        display.lcd_clear()
+        print("Keyboard interrupt detected.")
+        await amqp_connection.close()
+        display.lcd_display_string("closing", 1)
+        display.lcd_display_string("goodbye...", 2)
+        sleep(5)
+        display.lcd_clear()
 
     # except KeyboardInterrupt:
     #     display.lcd_clear()
@@ -106,7 +115,7 @@ async def connect_to_rabbitmq(amqp_connection):
         except Exception as e:
             print(e)
             display.lcd_clear()
-            show_on_lcd()
+            # show_on_lcd()
             display.lcd_display_string("Error, wait 1s", 1)
             display.lcd_display_string("Connecting...", 2)
             sleep(5)
@@ -115,8 +124,8 @@ async def connect_to_rabbitmq(amqp_connection):
 def show_info(message_json):
     # display.lcd_clear()
     th = f'T: {message_json["t"]}C H: {message_json["h"]}%'
-    # time = f'{get_formatted_date(message_json["tsp"])}'
-    # display.lcd_display_string(time, 1)
+    time = f'{get_formatted_date(message_json["tsp"])}'
+    display.lcd_display_string(time, 1)
     display.lcd_display_string(th, 2)
 
 
